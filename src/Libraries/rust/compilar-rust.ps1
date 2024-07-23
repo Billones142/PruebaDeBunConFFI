b25cd6896d76713a6f10244b0536b9de478fd910
Billones142 <stefanomerinoderui@gmail.com>
2024-07-23 20:09:31 -0300

    rust ahora tambien se compila para linux, zig agregado

--- src/Libraries/rust/compilar-rust.ps1
+++ src/Libraries/rust/compilar-rust.ps1
@@ -1,1 +1,8 @@
-rustc --crate-type cdylib mylib.rs
+# Build for Windows (.dll)
+rustc --crate-type=cdylib mylib.rs -o mylib.dll
+
+# Build for Linux (.so)
+rustc --crate-type=cdylib mylib.rs -o mylib.so
+
+# Build for macOS (.dylib)
+rustc --crate-type=cdylib mylib.rs -o mylib.dylib
