f580f2fee0003d6cdccf856747a48fd2026ec322
Billones142 <stefanomerinoderui@gmail.com>
2024-07-23 21:31:46 -0300

    cambio de estructuracion

--- src/index.ts
+++ src/index.ts
@@ -7,37 +7,29 @@ import { readdir } from "node:fs/promises";
 
 
 
-function executeFromLibrary(directoryFilePath : string, fileName : string) {
-  try {
-    // Verifica si el archivo DLL existe
-    const directoryAndFile= join(directoryFilePath,fileName).concat(`.${suffix}`);
-    if (!existsSync(directoryAndFile)) {
-      console.error(`The DLL file does not exist at path: ${directoryAndFile}`);
-      throw new Error("No exite el archivo");
-      ;
-    }
-    // crea una constante que se comporta como una funcion de Bun
-    const {
-      symbols: {
-        add, // Asegúrarse de que este nombre coincida con la función del DLL
-      },
-    } = dlopen(directoryAndFile, {
-      add: { // Asegúrarse de que este nombre coincida con la función del DLL
-        args: [FFIType.i32, FFIType.i32], // Tipos de argumentos
-        returns: FFIType.i32, // Tipo de retorno
-      },
-    });
-  
-    const result = add(5, 3);
-    console.log(directoryFilePath,`Result: ${result}`);
-  } catch (error) {
-    if (error.message !== 'No exite el archivo') {
-      console.log('error desconocido: "' + directoryFilePath +  '"');
-    }
+function executeFromLibrary(directoryFilePath : string, fileName : string) : number {
+  // Verifica si el archivo DLL existe
+  const directoryAndFile= join(directoryFilePath,fileName).concat(`.${suffix}`);
+  if (!existsSync(directoryAndFile)) {
+    throw new Error("The file doesn't exist");
+    ;
   }
+  // crea una constante que se comporta como una funcion de Bun
+  const {
+    symbols: {
+      add, // Asegúrarse de que este nombre coincida con la función del DLL
+    },
+  } = dlopen(directoryAndFile, {
+    add: { // Asegúrarse de que este nombre coincida con la función del DLL
+      args: [FFIType.i32, FFIType.i32], // Tipos de argumentos
+      returns: FFIType.i32, // Tipo de retorno
+    },
+  });
+
+  return add(5, 3);
 }
 
-async function buscarDirectorioEn(source:string) : Promise<string[]> {
+async function searchForDirectoryIn(source:string) : Promise<string[]> {
   return (await readdir(source, { recursive: false , withFileTypes: true}))
     .filter((fileOrDirectory) : boolean => {
       return fileOrDirectory.isDirectory()
@@ -48,15 +40,32 @@ async function buscarDirectorioEn(source:string) : Promise<string[]> {
   });
 }
 
-let source= join(__dirname,'./Libraries')
-let directories= await buscarDirectorioEn(source);
+async function executeAllLibraries() {
+  const source= join(__dirname,'./Libraries')
+  const directories= await searchForDirectoryIn(source);
+  const maxNameLength= (() : number => Math.max(...directories.map(str => str.length)))();
 
-console.log('Directorios de librerias encontrados:',directories);
+  console.log('Libraries Directories found:',directories);
 
-const filename = 'mylib';
+  const filename = 'mylib';
 
-for (let index = 0; index < directories.length ; index++) {
-  const libraryDirectory = resolve('src','Libraries',directories[index]);
-
-  executeFromLibrary(libraryDirectory,filename)
+  for (let index = 0; index < directories.length ; index++) {
+    const libraryDirectory = resolve('src','Libraries',directories[index]);
+    
+    let resultMessage : string;
+    try {
+      const result= executeFromLibrary(libraryDirectory,filename)
+      resultMessage= `Result: ${result}`;
+    } catch (error) {
+      resultMessage= error.message;
+    }
+    let spaces : string= '';
+    for (let i = directories[index].length; i < maxNameLength ; i++) {
+      spaces= spaces.concat(' ');
+    }
+    console.log(directories[index].concat(spaces), '| '.concat(resultMessage));
+    
+  }
 }
+
+await executeAllLibraries();
