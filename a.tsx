84da3204424b18c9343bae6f96302da68d070614
Billones142 <stefanomerinoderui@gmail.com>
2024-07-23 21:54:40 -0300

    limpieza de directorio

--- a.tsx
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-let clave: jwt.Secret= '4165156161'
-
-let permiso= jwt.sign({ hola: 'soy una cadena', tiempo: new Date()},clave)
-
-console.log(jwt.decode(permiso, {json: true}).tiempo);
